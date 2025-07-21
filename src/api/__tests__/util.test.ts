7ef677d557d7e6ac2a421a11cf8b98febb0d72cb
staniell <giostaniell12@gmail.com>
2025-07-22 04:51:49 +0800

    add UTs

--- src/api/__tests__/util.test.ts
+++ src/api/__tests__/util.test.ts
@@ -48,9 +48,9 @@ describe('Util tests', () => {
 
   describe('createWorkspace', () => {
     it('creates a new workspace', () => {
-      const workspace = utils.createWorkspace(testDbString)
+      const workspace = utils.createWorkspace(testDbString, 'testing')
       expect(workspace).toBeDefined()
-      expect(workspace.title).toEqual('')
+      expect(workspace.title).toEqual('testing')
       expect(workspace.buildShipments).toHaveLength(1)
       expect(workspace.buildShipments[0].shipments).toHaveLength(1)
       expect(workspace.buildShipments[0].buildNumber).toEqual('')
@@ -60,11 +60,96 @@ describe('Util tests', () => {
 
   describe('updateWorkspace', () => {
     it('updates a workspace', () => {
-      const workspace = utils.createWorkspace(testDbString)
+      const workspace = utils.createWorkspace(testDbString, 'testing')
       workspace.title = "Arnav's Shipping"
       utils.updateWorkspace(testDbString, workspace)
       const updatedWorkspace = utils.getWorkspace(testDbString, workspace.id)
       expect(updatedWorkspace.title).toEqual("Arnav's Shipping")
+    })
+  })
+
+  describe('deleteWorkspace', () => {
+    it('deletes a workspace', () => {
+      const workspace = utils.createWorkspace(testDbString, 'to be deleted')
+      const initialWorkspaces = utils.getWorkspaces(testDbString)
+      const success = utils.deleteWorkspace(testDbString, workspace.id)
+      expect(success).toBe(true)
+      const finalWorkspaces = utils.getWorkspaces(testDbString)
+      expect(finalWorkspaces.length).toBe(initialWorkspaces.length - 1)
+    })
+  })
+
+  describe('addShipment', () => {
+    it('adds a new shipment to an existing build', () => {
+      const newShipment = {
+        buildNumber: 'A82D2-108',
+        description: '128 units',
+        orderNumber: '121-5821131-5985043',
+        cost: 215286,
+      }
+      const updatedWorkspace = utils.addShipment(testDbString, workspaceId, newShipment)
+      expect(updatedWorkspace.buildShipments[0].shipments).toHaveLength(2)
+      expect(updatedWorkspace.buildShipments[0].shipments[1].description).toEqual('128 units')
+    })
+
+    it('adds a new shipment to a new build', () => {
+      const newShipment = {
+        buildNumber: 'B82D2-109',
+        description: '256 units',
+        orderNumber: '121-5821131-5985044',
+        cost: 430572,
+      }
+      const updatedWorkspace = utils.addShipment(testDbString, workspaceId, newShipment)
+      expect(updatedWorkspace.buildShipments).toHaveLength(2)
+      expect(updatedWorkspace.buildShipments[1].buildNumber).toEqual('B82D2-109')
+      expect(updatedWorkspace.buildShipments[1].shipments[0].description).toEqual('256 units')
+    })
+  })
+
+  describe('updateShipment', () => {
+    it('updates an existing shipment', () => {
+      const workspace = utils.getWorkspace(testDbString, workspaceId)
+      const shipmentToUpdate = workspace.buildShipments[0].shipments[0]
+      shipmentToUpdate.description = '32 units'
+      const updatedWorkspace = utils.updateShipment(testDbString, workspaceId, shipmentToUpdate)
+      expect(updatedWorkspace.buildShipments[0].shipments[0].description).toEqual('32 units')
+    })
+  })
+
+  describe('deleteShipment', () => {
+    it('deletes an existing shipment', () => {
+      const workspace = utils.getWorkspace(testDbString, workspaceId)
+      const shipmentIdToDelete = workspace.buildShipments[0].shipments[0].id
+      const success = utils.deleteShipment(testDbString, workspaceId, shipmentIdToDelete)
+      expect(success).toBe(true)
+      const updatedWorkspace = utils.getWorkspace(testDbString, workspaceId)
+      expect(updatedWorkspace.buildShipments[0].shipments).toHaveLength(0)
+    })
+  })
+
+  describe('updateBuildNumber', () => {
+    it('updates the build number of a build shipment', () => {
+      const workspace = utils.getWorkspace(testDbString, workspaceId)
+      const buildShipmentId = workspace.buildShipments[0].id
+      const newBuildNumber = 'C82D2-110'
+      const updatedWorkspace = utils.updateBuildNumber(
+        testDbString,
+        workspaceId,
+        buildShipmentId,
+        newBuildNumber
+      )
+      expect(updatedWorkspace.buildShipments[0].buildNumber).toEqual(newBuildNumber)
+    })
+  })
+
+  describe('deleteBuildShipment', () => {
+    it('deletes an existing build shipment', () => {
+      const workspace = utils.getWorkspace(testDbString, workspaceId)
+      const buildShipmentIdToDelete = workspace.buildShipments[0].id
+      const success = utils.deleteBuildShipment(testDbString, workspaceId, buildShipmentIdToDelete)
+      expect(success).toBe(true)
+      const updatedWorkspace = utils.getWorkspace(testDbString, workspaceId)
+      expect(updatedWorkspace.buildShipments).toHaveLength(0)
     })
   })
 })
